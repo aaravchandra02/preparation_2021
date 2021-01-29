02662502196dd9d0bc2c6413a24aa32395b92b75
Aarav Chandra <aaravchandra02@gmail.com>
2021-01-28 16:17:45 -0800

    added iterative method

--- Trees/inorder.py
+++ Trees/inorder.py
@@ -14,8 +14,20 @@ def inorder_recursive(root):
         inorder_recursive(root.right)
 
 
-def inorder_iterative():
-    pass
+def inorder_iterative(root):
+    if root == None:
+        return
+    s = []
+    while(True):
+        if not(root == None):
+            s.append(root)
+            root = root.left
+        else:
+            if(len(s) == 0):
+                break
+            root = s.pop()
+            print(root.data)
+            root = root.right
 
 
 root = Node(1)
@@ -23,5 +35,7 @@ root.left = Node(2)
 root.right = Node(3)
 root.left.left = Node(4)
 root.left.right = Node(5)
-print("\nInorder traversal of binary tree is")
+print("\n(Recursive)Inorder traversal of binary tree is:")
 inorder_recursive(root)
+print("\n(Iterative)Inorder traversal of binary tree is:")
+inorder_iterative(root)
