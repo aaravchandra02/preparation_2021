52e3b45934a9e3129ca35a855c7c048130bfee3e
Aarav Chandra <aaravchandra02@gmail.com>
2021-01-29 15:59:57 -0800

    added 2 stack iterative version

--- Trees/postorder.py
+++ Trees/postorder.py
@@ -11,23 +11,24 @@ def postorder_recursive(root):
         postorder_recursive(root.right)
         print(root.data)
 
-# To convert an inherently recursive procedures to iterative, we need an explicit stack
+# it uses 2 stacks
 
 
-def postorder_iterative(root):
+def postorder_iterative_2(root):
 
     if (root == None):
         return
-    nodeStack = []
-    nodeStack.append(root)
-    while(len(nodeStack)):
-        root = nodeStack.pop()
-        if(root.right is not None):
-            nodeStack.append(root.right)
-            continue
-        if(root.left is not None):
-            nodeStack.append(root.left)
-            continue
+    stack_1, stack_2 = [], []
+    stack_1.append(root)
+    while(len(stack_1)):
+        root = stack_1.pop()
+        stack_2.append(root)
+        if(root.left):
+            stack_1.append(root.left)
+        if(root.right):
+            stack_1.append(root.right)
+    while(len(stack_2)):
+        root = stack_2.pop()
         print(root.data)
 
 
@@ -38,5 +39,5 @@ root.left.left = Node(4)
 root.left.right = Node(5)
 print("\npostorder traversal of binary tree is")
 postorder_recursive(root)
-print(f"\n\n\n")
-print(postorder_iterative(root))
+print(f"\n\n")
+print(postorder_iterative_2(root))
