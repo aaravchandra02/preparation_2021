707f34d9a9f55663778784c14777de3f62aaeeec
Aarav Chandra <aaravchandra02@gmail.com>
2021-02-09 12:44:12 -0800

    added iterative solution

--- Trees/symmetric_tree.py
+++ Trees/symmetric_tree.py
@@ -48,6 +48,77 @@ print(f"\nSymmetric\n" if a.isSymmetric(
     root) == True else f"\nNot symmetric\n")
 
 
+class Solution_iterative:
+    # function to check if a given Binary Tree is symmetric or not
+    def isSymmetric(self, root):
+
+        # if tree is empty
+        if (root == None):
+            return True
+
+        # If it is a single tree node, then it is a symmetric tree.
+        if(not root.left and not root.right):
+            return True
+
+        q = []
+
+        # Add root to queue two times so that it can be checked if either one child alone is NULL or not.
+        q.append(root)
+        q.append(root)
+
+        # To store two nodes for checking their symmetry.
+        leftNode = 0
+        rightNode = 0
+
+        while(not len(q)):
+
+            # Remove first two nodes to check their symmetry.
+            leftNode = q[0]
+            q.pop(0)
+
+            rightNode = q[0]
+            q.pop(0)
+
+            # if both left and right nodes exist, but have different values-. inequality, return False
+            if(leftNode.data != rightNode.data):
+                return False
+
+            # append left child of left subtree node and right child of right subtree node in queue.
+            if(leftNode.left and rightNode.right):
+                q.append(leftNode.left)
+                q.append(rightNode.right)
+
+            # If only one child is present alone and other is NULL, then tree is not symmetric.
+            elif (leftNode.left or rightNode.right):
+                return False
+
+            # append right child of left subtree node and left child of right subtree node in queue.
+            if(leftNode.right and rightNode.left):
+                q.append(leftNode.right)
+                q.append(rightNode.left)
+
+            # If only one child is present alone and other is NULL, then tree is not symmetric.
+            elif(leftNode.right or rightNode.left):
+                return False
+
+        return True
+
+
+# Driver Code
+b = Solution_iterative()
+# Let us construct the Tree shown in the above figure
+root1 = TreeNode(1)
+root1.left = TreeNode(2)
+root1.right = TreeNode(2)
+root1.left.left = TreeNode(3)
+root1.left.right = TreeNode(4)
+root1.right.left = TreeNode(4)
+root1.right.right = TreeNode(3)
+if (b.isSymmetric(root1)):
+    print(f"\n\tThe given tree is Symmetric")
+else:
+    print(f"\n\tThe given tree is not Symmetric")
+
 # root = TreeNode(1, 2, 2)
 # root.left = TreeNode(2, 3, 3)
 # root.right = TreeNode(2, 3, 3)
